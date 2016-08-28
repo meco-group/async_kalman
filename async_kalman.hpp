a3114d978fa35f3795ef82872d9ff547331694d1
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-08-28 13:39:30 +0200

    Add propagation step

--- async_kalman.hpp
+++ async_kalman.hpp
@@ -11,7 +11,16 @@ typedef long long int f_int;
 
 // https://eigen.tuxfamily.org/dox/classEigen_1_1LLT.html
 
-void expm(const Md& A, double t, Md& Ae, Md& Aei, std::vector<f_int>& iwork, std::vector<double>& dwork);
+void slicot_mb05nd(int n, double delta, const double* A, int lda,
+                   double* ex, int ldex, double* exint, int ldexin,
+                   double tol, f_int* iwork, double* dwork, int ld_work, int& info);
+
+template<int N>
+void expm(const M<N, N>& A, double t, M<N, N>& Ae, M<N, N>& Aei, std::vector<f_int>& iwork, std::vector<double>& dwork) {
+  int info;
+  slicot_mb05nd(N, t, A.data(), N, Ae.data(), N, Aei.data(), N, 1e-7, &iwork[0], &dwork[0], 2*N*N, info);
+}
+
 
 /**
   x' = Ax + Bu + w
@@ -51,6 +60,82 @@ private:
   M<2*N, 2*N> Fei;
 };
 
-//(xp, Sp) = _filter_predict(Ad, np.linalg.cholesky(Qd),
-//                    np.array(Bd*u).squeeze(), np.array(x0).squeeze(),
-//                    S)
+template <int N, int Nu>
+class KalmanPropagator {
+public:
+  KalmanPropagator() {}
+
+  void propagate(const M<N, 1>& x, const M<N, N>& S, double t, M<N, 1>& xp, M<N, N>& Sp, const M<N, N>& A, const M<N, Nu>& B, const M<N, N>& Q, const M<Nu, 1>& u) {
+    // Discretize system
+    ki.integrate(A, B, Q, t, Ad, Bd, Qd);
+
+    // Propagate state
+    xp = Ad*x+Bd*u;
+
+    // Propagate covariance
+    Qf.compute(Qd);
+    SQ = Qf.matrixL();
+
+    Mm << (Ad*S).transpose(), SQ.transpose();
+    qr.compute(Mm);
+    Sp = qr.matrixQR().block(0, 0, N, N).template triangularView<Eigen::Upper>().transpose();
+  }
+
+private:
+
+  M<2*N, N> Mm;
+  Eigen::HouseholderQR< M<2*N, N> > qr;
+
+  KalmanIntegrator<N, Nu> ki;
+
+  M<N, N> Ad;
+  M<N, Nu> Bd;
+  M<N, N> Qd;
+  M<N, N> SQ;
+
+  Eigen::LLT< M<N, N> > Qf;
+};
+
+/**
++
++class KalmanObserver {
++public:
++  KalmanObserver(int n, int nb, int ny);
++
++  void observe(const M& x, const M& S, M& xp, M& Sp, const M& C, const M& D, const M& R, const M& z, const M& u);
++
++private:
++  int n_;
++  int nb_;
++  int ny_;
++
++  M Mm;
++  Eigen::HouseholderQR<M> qr;
++
++
++  M SR;
++
++  Eigen::LLT<M> Rf;
++
++  M zp;
++  M SZ;
++  M L;
++  M LZ;
++
++  Eigen::LLT<M> Sf;
++};
+
+
+template <int Nx, int Ny, int Nu>
+class SimpleMeasurement : public Measurment {
+  M2<Ny, Nx> C_;
+  M2<Ny, Nu> D_;
+  M2<Ny, Ny> R_;
+
+  void get(const M1<Nx> &x, const M2<Nx, Nx>& s, M2<Ny, Nx>& C, M2<Ny, Nu>& D, M2<Ny, Ny>& R) {
+    C = C_;
+    D = D_;
+    R = R_;
+  }
+};
+*/
--- tests.cpp
+++ tests.cpp
@@ -6,26 +6,44 @@ int main ( int argc , char *argv[] ) {
   std::cout << std::scientific;
   Eigen::IOFormat f(5, Eigen::DontAlignCols, " ", "\n", "[", "]", "[", "]");
 
-  M A(2, 2);
+  M<2, 2> A;
   A << 1  , 0.1,
        0.2, 1.1;
-  M B(2, 1);
+  M<2, 1> B;
   B << 3,
        4;
-  M Q(2, 2);
+  M<2, 2> Q;
   Q << 2, 0.1,
       0.1, 3;
 
-  KalmanIntegrator ki(2);
+  KalmanIntegrator<2, 1> ki;
 
-  M Ad(2, 2);
-  M Bd(2, 1);
-  M Qd(2, 2);
+  M<2, 2> Ad;
+  M<2, 1> Bd;
+  M<2, 2> Qd;
   ki.integrate(A, B, Q, 0.1, Ad, Bd, Qd);
 
   std::cout << Ad.format(f) << std::endl;
   std::cout << Bd.format(f) << std::endl;
   std::cout << Qd.format(f) << std::endl;
+
+  M<2, 1> x;
+  x << 1 , 0.1;
+
+  M<2, 2> S;
+  S << 3, 0,
+       2, 7;
+
+  M<1, 1> u(1, 1);
+  u << 2;
+
+  KalmanPropagator<2, 1> ks;
+  M<2, 1> xp(2, 1);
+  M<2, 2> Sp(2, 2);
+  ks.propagate(x, S, 0.1, xp, Sp, A, B, Q, u);
+
+  std::cout << xp.format(f) << std::endl;
+  std::cout << Sp.format(f) << std::endl;
 
   return 1;
 }
